388d383b9af8452614c2a2afcad655f1875d60e5
aryafirmansyah1 <aryfrmnsyh123@gmail.com>
2021-03-16 20:15:13 +0700

    projrct

---
+++ src/main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        main x = new main();
+        Persegi persegi = new Persegi(4);
+        Segitiga segitiga = new Segitiga(6, 3);
+        
+        System.out.println("Luas persegi: " + persegi.luas());
+        System.out.println("keliling persegi: " + persegi.keliling());
+        System.out.println("Luas segitiga: " + segitiga.luas());
+        
+    }
+    
+}
